b512e6830b517e85fff1b1e68a20cef7c37f0755
Lenus Walker <walker.lenus@gmail.com>
2022-10-24 19:03:26 -0400

    Update to use default windows Power Mode on the Dashboard and in the Automation Actions area.

--- LenovoLegionToolkit.Lib/Features/PowerModeFeature.cs
+++ LenovoLegionToolkit.Lib/Features/PowerModeFeature.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using LenovoLegionToolkit.Lib.Controllers;
 using LenovoLegionToolkit.Lib.Listeners;
@@ -12,6 +14,13 @@ namespace LenovoLegionToolkit.Lib.Features
     {
         private readonly AIModeController _aiModeController;
         private readonly PowerModeListener _listener;
+
+        private static readonly Dictionary<PowerModeState, string> defaultGenericPowerModes = new()
+        {
+            { PowerModeState.Quiet , "961cc777-2547-4f9d-8174-7d86181b8a7a"},
+            { PowerModeState.Balance , "00000000-0000-0000-0000-000000000000"},
+            { PowerModeState.Performance , "ded574b5-45a0-4f42-8737-46345c09c238"},
+        };
 
         public PowerModeFeature(AIModeController aiModeController, PowerModeListener listener)
             : base("SmartFanMode", 1, "IsSupportSmartFan")
@@ -29,22 +38,57 @@ namespace LenovoLegionToolkit.Lib.Features
             return new[] { PowerModeState.Quiet, PowerModeState.Balance, PowerModeState.Performance };
         }
 
+        public override async Task<PowerModeState> GetStateAsync()
+        {
+            var compatibility = await Compatibility.IsCompatibleAsync().ConfigureAwait(false);
+            if (compatibility.isCompatible)
+            {
+                return base.GetStateAsync().Result;
+            }
+            else
+            {
+                PowerModeState state = PowerModeState.Balance;
+                uint result = PowerGetEffectiveOverlayScheme(out Guid currentMode);
+                switch (currentMode.ToString())
+                {
+                    case "961cc777-2547-4f9d-8174-7d86181b8a7a":
+                        state = PowerModeState.Quiet;
+                        break;
+                    case "00000000-0000-0000-0000-000000000000":
+                        state = PowerModeState.Balance;
+                        break;
+                    case "ded574b5-45a0-4f42-8737-46345c09c238":
+                        state = PowerModeState.Performance;
+                        break;
+                }
+                return state;
+            }
+        }
+
         public override async Task SetStateAsync(PowerModeState state)
         {
             var allStates = await GetAllStatesAsync().ConfigureAwait(false);
             if (!allStates.Contains(state))
                 throw new InvalidOperationException($"Unsupported power mode {state}.");
 
-            var currentState = await GetStateAsync().ConfigureAwait(false);
+            var compatibility = await Compatibility.IsCompatibleAsync().ConfigureAwait(false);
+            if (compatibility.isCompatible)
+            {
+                var currentState = await GetStateAsync().ConfigureAwait(false);
 
-            await _aiModeController.StopAsync(currentState).ConfigureAwait(false);
+                await _aiModeController.StopAsync(currentState).ConfigureAwait(false);
 
-            // Workaround: Performance mode doesn't update the dGPU temp limit (and possibly other properties) on some Gen 7 devices.
-            var mi = await Compatibility.GetMachineInformationAsync().ConfigureAwait(false);
-            if (mi.Properties.HasPerformanceModeSwitchingBug && currentState == PowerModeState.Quiet && state == PowerModeState.Performance)
-                await base.SetStateAsync(PowerModeState.Balance).ConfigureAwait(false);
+                // Workaround: Performance mode doesn't update the dGPU temp limit (and possibly other properties) on some Gen 7 devices.
+                var mi = await Compatibility.GetMachineInformationAsync().ConfigureAwait(false);
+                if (mi.Properties.HasPerformanceModeSwitchingBug && currentState == PowerModeState.Quiet && state == PowerModeState.Performance)
+                    await base.SetStateAsync(PowerModeState.Balance).ConfigureAwait(false);
 
-            await base.SetStateAsync(state).ConfigureAwait(false);
+                await base.SetStateAsync(state).ConfigureAwait(false);
+            }
+            else
+            {
+                PowerSetActiveOverlayScheme(new Guid(defaultGenericPowerModes[state]));
+            }
 
             await _listener.NotifyAsync(state).ConfigureAwait(false);
         }
@@ -61,14 +105,38 @@ namespace LenovoLegionToolkit.Lib.Features
 
         public async Task EnsureAIModeIsSetAsync()
         {
-            var state = await GetStateAsync().ConfigureAwait(false);
-            await _aiModeController.StartAsync(state).ConfigureAwait(false);
+            var compatibility = await Compatibility.IsCompatibleAsync().ConfigureAwait(false);
+            if (compatibility.isCompatible)
+            {
+                var state = await GetStateAsync().ConfigureAwait(false);
+                await _aiModeController.StartAsync(state).ConfigureAwait(false);
+            }
         }
 
         public async Task EnsureAIModeIsOffAsync()
         {
-            var state = await GetStateAsync().ConfigureAwait(false);
-            await _aiModeController.StopAsync(state).ConfigureAwait(false);
+            var compatibility = await Compatibility.IsCompatibleAsync().ConfigureAwait(false);
+            if (compatibility.isCompatible)
+            {
+                var state = await GetStateAsync().ConfigureAwait(false);
+                await _aiModeController.StopAsync(state).ConfigureAwait(false);
+            }
         }
+
+        /// <summary>
+        /// Retrieves the active overlay power scheme and returns a GUID that identifies the scheme.
+        /// </summary>
+        /// <param name="EffectiveOverlayPolicyGuid">A pointer to a GUID structure.</param>
+        /// <returns>Returns zero if the call was successful, and a nonzero value if the call failed.</returns>
+        [DllImportAttribute("powrprof.dll", EntryPoint = "PowerGetEffectiveOverlayScheme")]
+        private static extern uint PowerGetEffectiveOverlayScheme(out Guid EffectiveOverlayPolicyGuid);
+
+        /// <summary>
+        /// Sets the active power overlay power scheme.
+        /// </summary>
+        /// <param name="OverlaySchemeGuid">The identifier of the overlay power scheme.</param>
+        /// <returns>Returns zero if the call was successful, and a nonzero value if the call failed.</returns>
+        [DllImportAttribute("powrprof.dll", EntryPoint = "PowerSetActiveOverlayScheme")]
+        private static extern uint PowerSetActiveOverlayScheme(Guid OverlaySchemeGuid);
     }
 }
