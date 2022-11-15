3bcf25376de82aec0f65871e5f01fe8a19a6708e
Lenus Walker <walker.lenus@gmail.com>
2022-11-15 10:48:07 -0400

    remove unnecessary usings.

--- LenovoLegionToolkit.Lib/Features/DisplayBrightnessFeature.cs
+++ LenovoLegionToolkit.Lib/Features/DisplayBrightnessFeature.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Linq;
 using System.Threading.Tasks;
-using LenovoLegionToolkit.Lib.Controllers;
 using LenovoLegionToolkit.Lib.System;
 using LenovoLegionToolkit.Lib.Utils;
 using WindowsDisplayAPI;
